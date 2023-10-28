14d27549a8ea8a909e87c6715f9b9a65ba075f0b
amuhirwa <a.muhirwa@alustudent.com>
2023-10-28 05:21:15 -0700

    Task done

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from sys import exit,argv
+from sys import exit, argv
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    if len(argv) < 3:
+    if len(argv) < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
