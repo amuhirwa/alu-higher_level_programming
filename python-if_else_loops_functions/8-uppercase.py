812d351b27771a9dee511b73f998ffe266837147
amuhirwa <a.muhirwa@alustudent.com>
2023-10-28 02:19:27 -0700

    Task done

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for count, i in enumerate(str):
-        if i in range(97, 123):
+        if ord(i) in range(97, 123):
             chars = chr(ord(i) - 32)
         else:
             chars = i
