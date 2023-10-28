f2c395ee139bd28e5f53dff39c8c96debdd80d70
amuhirwa <a.muhirwa@alustudent.com>
2023-10-28 03:30:38 -0700

    Task done

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        fizz = str(i)
-        buzz = ""
+        fizz = ""
+        buzz = str(i)
         if i%3 == 0:
-            fizz = "fizz"
+            fizz = "Fizz"
+            buzz = ""
         if i%5 == 0:
-            buzz = "buzz"
+            buzz = "Buzz"
         
         print(fizz+buzz,end=" ")
