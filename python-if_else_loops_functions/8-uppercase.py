6e64764ccbd03ee32911702561b79e475a84189e
amuhirwa <a.muhirwa@alustudent.com>
2023-10-28 02:12:23 -0700

    Task done

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for count,i in enumerate(str):
+        if i in range(65,91):
+            chars=i
+        elif i in range(97,123):
+            chars=chr(ord(i)-32)
+        if count==len(str)-1:
+            print(chars)
+        else:
+            print(chars,end="")
