297f11ce2c0b54fab3f43ad5be4e914f7d56d4fa
amuhirwa <a.muhirwa@alustudent.com>
2023-11-04 16:29:14 -0700

    Task done

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(strs):
-    if type(strs) != "str":
+    if type(strs) is not str:
         return 0
     roms = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     sum = 0
