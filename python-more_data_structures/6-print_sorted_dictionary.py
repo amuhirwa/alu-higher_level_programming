d4d22509c599bb827b7605ec45a05d16a5da722c
amuhirwa <a.muhirwa@alustudent.com>
2023-11-04 08:56:13 -0700

    Task done

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
+def print_sorted_dictionary(a_dictionary):
     x = sorted(a_dictionary.keys())
     for i in x:
         print(f'{i}: {a_dictionary[i]}')
