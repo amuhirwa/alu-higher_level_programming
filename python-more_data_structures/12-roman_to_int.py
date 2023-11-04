ce44152472ae28ae2094cbbd4260b93ad6705172
amuhirwa <a.muhirwa@alustudent.com>
2023-11-04 16:26:25 -0700

    Task done

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(strs):
-    rom_nums = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    if type(strs) != "str":
+        return 0
+    roms = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     sum = 0
     minus = 0
     for i in range(len(strs)):
         if i != len(strs) - 1:
-            if rom_nums[strs[i]] < rom_nums[strs[i+1]]:
-                minus = -(rom_nums[strs[i]])
+            if roms[strs[i]] < roms[strs[i+1]]:
+                minus = -(roms[strs[i]])
             else:
-                sum += minus + rom_nums[strs[i]]
+                sum += minus + roms[strs[i]]
                 minus = 0
         else:
-            sum += minus + rom_nums[strs[i]]
+            sum += minus + roms[strs[i]]
     return sum
