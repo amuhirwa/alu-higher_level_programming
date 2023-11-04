3731dbc719b8f53f5fe464c11d90d479cb060f9a
amuhirwa <a.muhirwa@alustudent.com>
2023-11-04 04:05:39 -0700

    Task done

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+def no_c(my_string):
+    list_string = list(my_string)
+    list_string = [i for i in list_string if i.lower() != 'c']
+    return ''.join(list_string)
---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    for i in [tuple_a,tuple_b]:
+        i += (0,0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
