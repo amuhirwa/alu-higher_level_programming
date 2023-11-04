b10685c4f90a06efffc018cd6f92df47059a2bfc
amuhirwa <a.muhirwa@alustudent.com>
2023-11-04 16:56:48 -0700

    Task done

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list == []:
+        return 0
     weight_tot = list(map(lambda x: x[0] * x[1], my_list))
     weights = sum([i[1] for i in my_list])
     return sum(weight_tot) / weights
