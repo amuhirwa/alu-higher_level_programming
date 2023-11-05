13f8657d4dee7678cbf5b9c57f39eabf974c90f6
amuhirwa <a.muhirwa@alustudent.com>
2023-11-05 10:02:52 -0800

    Task done

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -3,6 +3,6 @@ def safe_function(fct, *args):
     import sys
     try:
         return fct(*args)
-    except Exceptionn as err:
+    except Exception as err:
         print(f"Exception: {e}", file=sys.stderr)
         return None
