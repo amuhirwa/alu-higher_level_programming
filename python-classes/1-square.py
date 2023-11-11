a1d265f4db3f77bac5a251c3f2eba0c20e3ddc08
amuhirwa <a.muhirwa@alustudent.com>
2023-11-11 13:34:18 -0800

    Task done

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module to define a square class"""
+
+
+class Square:
+    """Class to define a square"""
+    
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
