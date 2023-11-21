4e4f52cc39f46248914b4f15bd65718e10f37c26
amuhirwa <a.muhirwa@alustudent.com>
2023-11-21 18:32:37 +0200

    Task done

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,3 +6,5 @@ def write_file(filename="", text=""):
     """Function to write to a text file"""
     with open(filename, "w+", encoding="utf-8") as f:
         f.write(text)
+        f.seek(0)
+        return len(f.read())
