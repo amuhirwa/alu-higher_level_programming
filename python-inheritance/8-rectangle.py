3a0c9c174eece337fc2c7d466d4a28c0c591d84e
amuhirwa <a.muhirwa@alustudent.com>
2023-11-21 13:24:56 +0200

    Task done

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module to design a rectangle"""
+
+
+class BaseGeometry:
+    """Represents base geometry"""
+    def area(self):
+        """Method to calculate raise exception for now"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method to validate that value is a valid integer"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle"""
+    def __init__(self, width, height):
+        """Initializes width and height of Rectangle
+            Args:
+                width(int): value of width of rectangle
+                height(int): value of height of rectangle
+        """
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, self.__width, self.__height)
