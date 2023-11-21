de7eb8f0beb0a06c483a43bd3c84b3b272a7289e
amuhirwa <a.muhirwa@alustudent.com>
2023-11-21 12:54:30 +0200

    Task done

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,11 @@
 
 
 def is_kind_of_class(obj, a_class):
+    """Function to check if instance inherits from certain class
+     or is of that class"""
     if type(obj) == a_class:
         return True
     if type(obj).__base__ == a_class:
         return True
+    print(obj, a_class )
     return False
--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module to create a class called BaseGeometry"""
 
+
 class BaseGeometry:
     """Represents BaseGeometry"""
