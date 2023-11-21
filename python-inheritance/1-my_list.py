0ff3397b65d39e3a9b430401fbe69cc81876ad20
amuhirwa <a.muhirwa@alustudent.com>
2023-11-21 12:08:39 +0200

    Task done

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,9 +3,11 @@
 
 
 class MyList(list):
-    """Subclass of list"""
+    """a subclass of list"""
     def __init__(self):
+        """initializes the object"""
         super().__init__()
 
     def print_sorted(self):
+        """prints the sorted list"""
         print(sorted(self))
