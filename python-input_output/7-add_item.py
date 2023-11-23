6bd1919621d7b28f10082e40efe25ab61314c980
amuhirwa <a.muhirwa@alustudent.com>
2023-11-23 23:24:10 +0200

    Task done

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that adds all arguments to a Python list, and then saves them to a file"""
+to_json = __import__("5-save_to_json_file.py").save_to_json_file
+from_json = __import__("6-load_from_json_file.py").load_from_json_file
+from sys import argv as arg
+
+filename = "add_item.json"
+with open(filename, "a+") as f:
+    if len(f.read()) == 0:
+        arr = []
+    else:
+        arr = from_json(f)
+        for i in arg:
+            arr.append(i)
