561b53995006c8520a6a5ed90cae49c867da4f51
amuhirwa <a.muhirwa@alustudent.com>
2023-11-29 09:08:49 -0800

    Task done

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends post data
-curl -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}' -s $1
+curl -d 'email=test@gmail.com&subject=I will always be here for PLD' -s $1
