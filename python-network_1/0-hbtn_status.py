fcb178732ba3e16173cbe15757ea09c33f2ff417
amuhirwa <a.muhirwa@alustudent.com>
2023-12-01 11:08:17 -0800

    Task done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """Prints status of site"""
 import urllib.request
+import sys
 
-with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+url = 'https://intranet.hbtn.io/status'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
+    '\n    Chrome/99.0.4844.84 Safari/537.36',
+}
+
+req = urllib.request.Request(url, headers=headers)
+with urllib.request.urlopen(req) as response:
     content = response.read()
     print("Body response:")
     print("\t- type:", type(content))
