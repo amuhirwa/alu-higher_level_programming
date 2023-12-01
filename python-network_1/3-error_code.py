be0b6554b05ec6070238df5c993e38d60661c5ca
amuhirwa <a.muhirwa@alustudent.com>
2023-12-01 06:03:46 -0800

    Task done

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         with request.urlopen(sys.argv[1]) as r:
             print(r.read().decode('UTF-8'))
     except error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        print("Error code:", e.code)
