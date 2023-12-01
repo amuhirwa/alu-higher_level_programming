38b9f3fba7945c99a6df1e240fd531e03e1b1f9c
amuhirwa <a.muhirwa@alustudent.com>
2023-12-01 13:31:10 -0800

    Task done

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     auth = HTTPBasicAuth(username=sys.argv[1], password=sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
     result = r.json()
-    print(result.id)
+    print(result.get("id"))
