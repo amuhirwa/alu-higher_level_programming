eeb6bb0fce983782056b4224b5379be3c156dcd6
amuhirwa <a.muhirwa@alustudent.com>
2023-12-01 06:00:32 -0800

    Task done

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -7,9 +7,9 @@ Module displays body of response
 if __name__ == "__main__":
     import sys
     from urllib import request, error
-    
+
     try:
-        with request.urlopen(sys.argv[1]) as r:
-            print(r.read().decode('UTF-8'))
-    except error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
