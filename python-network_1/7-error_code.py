ef44ef2132e60282e4aa0aef247dc8470ae5e8ad
amuhirwa <a.muhirwa@alustudent.com>
2023-12-01 13:21:02 -0800

    Task done

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     
-    if r.code >= 400:
-        print("Error code:", r.code)
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
     else:
         print(r.text)
