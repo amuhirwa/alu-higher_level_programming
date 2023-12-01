3b8d1e5e85223d530254896b1971a8930d37cadb
amuhirwa <a.muhirwa@alustudent.com>
2023-12-01 13:29:04 -0800

    Task done

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module to log into github"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(username=sys.argv[1], password=sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    result = r.json()
+    print(result)
