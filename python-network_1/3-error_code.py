7aa88fb9a0ecb57cd8b08731133d82f4f6ef5e0c
amuhirwa <a.muhirwa@alustudent.com>
2023-12-01 05:44:49 -0800

    Task done

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
 """
+Module displays body of response
+"""
+
 
 if __name__ == "__main__":
     from urllib import request, error
