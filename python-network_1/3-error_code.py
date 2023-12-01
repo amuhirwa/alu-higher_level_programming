104c02fe1c00c7f3da20c7f16336536bcada8d21
amuhirwa <a.muhirwa@alustudent.com>
2023-12-01 05:33:13 -0800

    Task done

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""
-Module to display body of response or error code
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
 """
 
+
 if __name__ == "__main__":
-    from urllib import request, error
     import sys
-    url = sys.argv[1]
-    req = request.Request(url)
+    from urllib import request, error
+
     try:
-        with request.urlopen(req) as r:
-            print(r.read().decode())
-    except error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
