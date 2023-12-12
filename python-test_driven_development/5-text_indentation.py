9ed88b2a37bd6ba2eb271aeb9b910fd5d8b01e17
amuhirwa <a.muhirwa@alustudent.com>
2023-12-11 17:14:54 -0800

    Task done

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """Module to print text with indentation"""
 
+
 def text_indentation(text):
     """Function to print text"""
     if type(text) != str:
         raise TypeError("text must be a string")
     text2 = text.strip()
     new_text = text2[0]
-    ignore_space = False
     for i in text2[1:]:
         if i in '.?:':
             new_text += i + "\n\n"
-            ignore_space = True
         else:
-            if i == " " and ignore_space is True:
-                ignore_space = False
+            if i == " " and new_text[-1] == "\n":
                 continue
             new_text += i
-            ignore_space = False
     print(new_text, end="")
