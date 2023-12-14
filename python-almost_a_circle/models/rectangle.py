e48980f18e4c1b911288a0db62c6e2f95a8f9a73
amuhirwa <a.muhirwa@alustudent.com>
2023-12-14 02:50:35 -0800

    Task done

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,7 +75,8 @@ class Rectangle(Base):
 
     def display(self):
         """Displays # representation"""
-        [print("#" * self.__width) for i in range(self.__height)]
+        [print() for i in range(self.__x)]
+        [print(" "*self.__y + "#" * self.__width) for i in range(self.__height)]
 
     def __str__(self):
         """Shows attributes of rectangle"""
