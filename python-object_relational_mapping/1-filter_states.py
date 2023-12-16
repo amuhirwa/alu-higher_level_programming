e47995d1f1cfc08c7bf07ae7fbe9bd335fe63f98
amuhirwa <a.muhirwa@alustudent.com>
2023-12-16 15:23:19 -0800

    Task done

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module to display certain states"""
+import sys
+import MySQLdb as mysql
+
+if __name__ == "__main__":
+    connector = mysql.connect(host="localhost", port=3306,
+                              host=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = connector.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    x = cur.fetchall()
+    for i in x:
+        print(x)
+    cur.close()
+    connector.close()
