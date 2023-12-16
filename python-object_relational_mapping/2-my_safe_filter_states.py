ca7ac48ef6a4804465bfab1d4640456345898cfe
amuhirwa <a.muhirwa@alustudent.com>
2023-12-16 15:43:08 -0800

    Task done

--- python-object_relational_mapping/2-my_safe_filter_states.py
+++ python-object_relational_mapping/2-my_safe_filter_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Module displays certain states according to input"""
+"""Module to display states according to input but safe"""
 import sys
 import MySQLdb as mysql
 
 if __name__ == "__main__":
+    if ';' in sys.argv[4]:
+        exit()
     connector = mysql.connect(host="localhost", port=3306,
                               user=sys.argv[1], passwd=sys.argv[2],
                               db=sys.argv[3])
     cur = connector.cursor()
-    cur.execute("SELECT * FROM states WHERE name={} "
-                "ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name={}"
+                " ORDER BY id ASC".format(sys.argv[4]))
     x = cur.fetchall()
     for i in x:
         print(i)
