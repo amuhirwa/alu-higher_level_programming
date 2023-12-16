e596fc13b615318c6930139a1eade965d6830f48
amuhirwa <a.muhirwa@alustudent.com>
2023-12-16 15:52:35 -0800

    Task done

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                               user=sys.argv[1], passwd=sys.argv[2],
                               db=sys.argv[3])
     cur = connector.cursor()
-    cur.execute("SELECT * FROM states WHERE name={}"
+    cur.execute("SELECT * FROM states WHERE name='{}'"
                 " ORDER BY id ASC".format(sys.argv[4]))
     x = cur.fetchall()
     for i in x:
