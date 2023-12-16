1e76d562a9bbb96d8e0716ef2f9ecc8b1ab80f71
amuhirwa <a.muhirwa@alustudent.com>
2023-12-16 15:29:17 -0800

    Task done

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module displays certain states according to input"""
+import sys
+import MySQLdb as mysql
+
+if __name__ == "__main__":
+    connector = mysql.connect(host="localhost", port=3306,
+                              user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = connector.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name={sys.argv[4]} ORDER BY id ASC")
+    x = cur.fetchall()
+    for i in x:
+        print(i)
+    cur.close()
+    connector.close()
