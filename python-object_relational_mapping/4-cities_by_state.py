2916ef0d87cf816fc338d038ca4c49c8db826874
amuhirwa <a.muhirwa@alustudent.com>
2023-12-17 03:49:14 -0800

    Task done

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
                               user=sys.argv[1], passwd=sys.argv[2],
                               db=sys.argv[3])
     cur = connector.cursor()
-    cur.execute("SELECT * FROM states INNER JOIN cities "
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM states INNER JOIN cities "
                 "ON states.id=cities.state_id"
                 " ORDER BY cities.id ASC")
     x = cur.fetchall()
