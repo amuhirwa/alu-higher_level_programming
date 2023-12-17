bb6fe6deea16375f62e16db3a9172cceba50ee44
amuhirwa <a.muhirwa@alustudent.com>
2023-12-17 03:41:24 -0800

    Task done

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,8 +4,6 @@ import sys
 import MySQLdb as mysql
 
 if __name__ == "__main__":
-    if ';' in sys.argv[4]:
-        exit()
     connector = mysql.connect(host="localhost", port=3306,
                               user=sys.argv[1], passwd=sys.argv[2],
                               db=sys.argv[3])
