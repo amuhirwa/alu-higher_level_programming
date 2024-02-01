6223c9faeabc312545656b208aab534c92db1ca6
amuhirwa <a.muhirwa@alustudent.com>
2024-02-01 20:40:23 +0200

    Task done

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 const myVar = "JavaScript is amazing";
 console.log(myVar);
+console.log("“C is fun”"+
+"The second line: “Python is cool”" +
+"The third line: “JavaScript is amazing”")
---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+console.log("“C is fun”"+
+"The second line: “Python is cool”" +
+"The third line: “JavaScript is amazing”")
+
