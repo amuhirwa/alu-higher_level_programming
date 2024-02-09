56616e0df249493ecd2b41c37518b419f6de6861
amuhirwa <a.muhirwa@alustudent.com>
2024-02-09 13:23:40 +0200

    Task donew

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const path = process.argv.slice(2)[0];
-fs.readFile(path, (err, data) => {
+fs.readFile(path, 'utf-8' , (err, data) => {
   if (err) {
     console.log(err);
   } else {
